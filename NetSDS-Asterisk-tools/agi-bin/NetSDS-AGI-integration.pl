0b280ac60823722f814a73baf6639c7672b3a95f
Alex Radetsky <rad@rad.kiev.ua>
2011-12-30 16:45:17 +0200

    added TaxiOffice integration open_blank method

--- NetSDS-Asterisk-tools/agi-bin/NetSDS-AGI-integration.pl
+++ NetSDS-Asterisk-tools/agi-bin/NetSDS-AGI-integration.pl
@@ -3,9 +3,9 @@
 #
 #         FILE:  NetSDS-AGI-integration.pl
 #
-#        USAGE:  ./NetSDS-AGI-integration.pl 
+#        USAGE:  ./NetSDS-AGI-integration.pl
 #
-#  DESCRIPTION:  Integration scripts. Call it in Queue parameters. 
+#  DESCRIPTION:  Integration scripts. Call it in Queue parameters.
 #
 #      OPTIONS:  ---
 # REQUIREMENTS:  ---
@@ -38,11 +38,11 @@ Integration->run(
 package Integration;
 
 use base qw(NetSDS::App);
-use Data::Dumper; 
-use Asterisk::AGI; 
+use Data::Dumper;
+use Asterisk::AGI;
 
-sub start { 
-	my $this = shift; 
+sub start {
+    my $this = shift;
 
     $this->mk_accessors('dbh');
     $this->mk_accessors('agi');
@@ -50,9 +50,8 @@ sub start {
     $this->agi( new Asterisk::AGI );
     $this->agi->ReadParse();
 
-	$this->_db_connect; 
+    $this->_db_connect;
 }
-
 
 sub _db_connect {
     my $this = shift;
@@ -94,20 +93,20 @@ sub _db_connect {
     return 1;
 }
 
-sub _exit { 
-	my $this = shift; 
-	my $errstr = shift; 
-	
-	$this->log("warning",$errstr);
-	$this->agi->verbose($errstr,3); 
-	exit(-1);
+sub _exit {
+    my $this   = shift;
+    my $errstr = shift;
+
+    $this->log( "warning", $errstr );
+    $this->agi->verbose( $errstr, 3 );
+    exit(-1);
 }
 
-sub _get_memberinterface { 
-	my $this = shift; 
+sub _get_memberinterface {
+    my $this = shift;
 
-	my $memberinterface = $this->agi->get_variable('MEMBERINTERFACE');
-	return $memberinterface;  	
+    my $memberinterface = $this->agi->get_variable('MEMBERINTERFACE');
+    return $memberinterface;
 
 }
 
@@ -121,58 +120,145 @@ sub _begin {
     }
 }
 
-sub _find_sipid_by_name { 
-	my $this = shift; 
-	my $sip_name = shift; 
+sub _find_sipid_by_name {
+    my $this     = shift;
+    my $sip_name = shift;
 
-	$this->_begin; 
+    $this->_begin;
 
-	my $sth = $this->dbh->prepare ("select id,ipaddr from public.sip_peers where name=? order by id asc limit 1");
-    eval { 
-		my $rv = $sth->execute ($sip_name); 
-	}; 
-	if ($@) { 
-		$this->_exit ( $this->dbh->errstr); 
-	} 
-	my $result = $sth->fetchrow_hashref; 
-	unless ( defined ( $result ) ) { 
-		$this->_exit("CAN'T FIND SIP/$sip_name");
-	}
+    my $sth = $this->dbh->prepare(
+"select id,ipaddr from public.sip_peers where name=? order by id asc limit 1"
+    );
+    eval { my $rv = $sth->execute($sip_name); };
+    if ($@) {
+        $this->_exit( $this->dbh->errstr );
+    }
+    my $result = $sth->fetchrow_hashref;
+    unless ( defined($result) ) {
+        $this->_exit("CAN'T FIND SIP/$sip_name");
+    }
 
-	my $id = $result->{'id'}; 
-	my $ipaddr = $result->{'ipaddr'}; 
-	
-	$this->dbh->commit; 
-	return ($id, $ipaddr); 
+    my $id     = $result->{'id'};
+    my $ipaddr = $result->{'ipaddr'};
 
-}
-
-sub process { 
-	my $this = shift; 
-
-# Get member interface 
-
-	my $memberinterface = $this->_get_memberinterface; 
-	unless ( defined ( $memberinterface ) ) { 
-		$this->_exit("CAN'T FIND MEMBERINTERFACE VALUE");
-	}
-	$this->log("info","Member interface: $memberinterface");
-	$this->agi->verbose("Member interface: $memberinterface",3);
-
-	$memberinterface =~ /^SIP\/(.*)$/; 
-	my $sip_name = $1; 
-
-# Get SIP ID + addr
-
-	my ($sip_id, $sip_addr) = $this->_find_sipid_by_name ($sip_name); 
-	
-# GET Computer integration info 
-
+    $this->dbh->commit;
+    return ( $id, $ipaddr );
 
 }
 
+sub _get_iinfo {
+    my $this     = shift;
+    my $sip_id   = shift;
+    my $sip_addr = shift;
+
+    $this->_begin;
+
+    my $sth = $this->dbh->prepare(
+"select * from integration.workplaces where sip_id=? and ip_addr_tel=? order by id asc limit 1"
+    );
+    my $sth2 = $this->dbh->prepare(
+"select * from integration.workplaces where sip_id=? order by id asc limit 1"
+    );
+
+    eval { my $rv = $sth->execute( $sip_id, $sip_addr ); };
+    if ($@) {
+        $this->_exit( $this->dbh->errstr );
+    }
+    my $result = $sth->fetchrow_hashref;
+    unless ( defined($result) ) {
+        $this->agi->verbose(
+"CAN'T FIND INTEGRATION INFO FOR SIP ID=$sip_id AND IP ADDR='$sip_addr'. TRYING WITHOUT IP",
+            3
+        );
+        $this->log( "warning",
+"CAN'T FIND INTEGRATION INFO FOR SIP ID=$sip_id AND IP ADDR='$sip_addr'. TRYING WITHOUT IP"
+        );
+
+        eval { my $rv = $sth2->execute($sip_id); };
+        if ($@) {
+            $this->_exit( $this->dbh->errstr );
+        }
+        $result = $sth->fetchrow_hashref;
+        unless ( defined($result) ) {
+            $this->_exit("CAN'T FIND INTEGRATION INFO FOR SIP ID=$sip_id");
+        }
+        $this->dbh->commit;
+        return $result;
+    }
+    $this->dbh->commit;
+    return $result;
+
+}
+
+sub _open_blank_taxi_office {
+    my $this  = shift;
+    my $iinfo = shift;
+
+    my $socket = IO::Socket::INET->new(
+        PeerAddr => $iinfo->{'ip_addr_pc'},
+        PeerPort => $iinfo->{'port'},
+        Proto    => "tcp",
+        Timeout  => 1
+    );
+
+    unless ($socket) {
+        $this->_exit( "CAN'T CONNECT TO "
+              . $iinfo->{'ip_addr_pc'} . ":"
+              . $iinfo->{'port'} );
+    }
+
+    my $callerid = $this->agi->get_variable("CALLERID(num)");
+    my $uline    = $this->agi->get_variable("PARKINGEXTEN");
+
+    my $command = sprintf( "COF\r\n%s\r\n-\r\n%s", $callerid, $uline );
+    if ( $socket->print($command) ) {
+        $socket->flush;
+    }
+    else {
+        $this->_exit( "CAN'T WRITE TO THE SOCKET "
+              . $iinfo->{'ip_addr_pc'} . ":"
+              . $iinfo->{'port'} );
+    }
+    undef $socket;
+
+}
+
+sub process {
+    my $this = shift;
+
+    # Get member interface
+
+    my $memberinterface = $this->_get_memberinterface;
+    unless ( defined($memberinterface) ) {
+        $this->_exit("CAN'T FIND MEMBERINTERFACE VALUE");
+    }
+    $this->log( "info", "Member interface: $memberinterface" );
+    $this->agi->verbose( "Member interface: $memberinterface", 3 );
+
+    $memberinterface =~ /^SIP\/(.*)$/;
+    my $sip_name = $1;
+
+    # Get SIP ID + addr
+
+    my ( $sip_id, $sip_addr ) = $this->_find_sipid_by_name($sip_name);
+
+    # GET Computer integration info
+
+    my $iinfo = $this->_get_iinfo( $sip_id, $sip_addr );
+
+    # Check the type of integration and execute it to Computer .
+
+    my $itype = $iinfo->{'integration_type'};
+
+    # Currently we support only TaxiOffice mode.
+
+    if ( $itype =~ /^TaxiOffice$/i ) {
+        $this->_open_blank_taxi_office($itype);
+    }
+}
 
 1;
+
 #===============================================================================
 
 __END__
